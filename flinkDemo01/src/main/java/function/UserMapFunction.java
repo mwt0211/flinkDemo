4ea597f6f45c35f03a8b4e3236dfdc67e3803f19
“mwt0211” <mwt17537630237@163.com>
2025-04-24 11:20:29 +0800

    事件窗口，水位线

--- flinkDemo01/src/main/java/function/UserMapFunction.java
+++ flinkDemo01/src/main/java/function/UserMapFunction.java
@@ -8,11 +8,11 @@ public class UserMapFunction implements MapFunction<String, User> {
     public User map(String value) throws Exception {
         String[] split = value.split(",");
         User user = new User();
-        user.setId(split[0]);
-        user.setName(split[1]);
-        user.setAge(Integer.valueOf(split[2]));
-        user.setAddress(split[3]);
-        user.setEmail(split[4]);
+//        user.setId(split[0]);
+        user.setName(split[0]);
+        user.setAge(Integer.valueOf(split[1]));
+//        user.setAddress(split[3]);
+//        user.setEmail(split[4]);
 
         return user;
     }
---
+++ flinkDemo01/src/main/java/waterMark/waterMarkDemo1.java
@@ -0,0 +1,90 @@
+package waterMark;
+
+import function.UserMapFunction;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkGenerator;
+import org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.util.Collector;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import pojo.User;
+
+import java.util.Properties;
+
+/**
+ * 水位线，有序
+ *
+ * */
+public class waterMarkDemo1 {
+    public static void main(String[] args) throws Exception {
+        /**
+         *
+         * 水位线
+         * */
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // Kafka broker地址
+        FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<>(
+                "test",
+                new SimpleStringSchema(),
+                properties);
+        //从最新处消费
+        consumer.setStartFromLatest();
+        env.setParallelism(1);
+        DataStreamSource<String> stream = env.addSource(consumer);
+        SingleOutputStreamOperator<User> userDs = stream.map(new UserMapFunction());
+
+        //todo:指定waterMark策略
+        WatermarkStrategy<User> userWatermarkStrategy = WatermarkStrategy
+                .<User>forMonotonousTimestamps()//指定升序，无等待时间
+                //
+                .withTimestampAssigner(new SerializableTimestampAssigner<User>() {
+            @Override
+            public long extractTimestamp(User element, long recordTimestamp) {
+                System.out.println("数据 " + element+"=====>  时间戳： "+recordTimestamp);
+                return element.getAge() * 1000l;
+            }
+        });
+        SingleOutputStreamOperator<User> userDsWaterMark = userDs.assignTimestampsAndWatermarks(userWatermarkStrategy);
+        KeyedStream<User, String> userKs = userDsWaterMark.keyBy(User::getName);
+        /**
+         * 指定事件时间语义窗口,waterMark对事件时间起作用
+         * 1：
+         * */
+        WindowedStream<User, String, TimeWindow> userWs = userKs.window(TumblingEventTimeWindows.of(Time.seconds(10)));
+
+        SingleOutputStreamOperator<String> process = userWs.process(new ProcessWindowFunction<User, String, String, TimeWindow>() {
+            @Override
+            public void process(String s, ProcessWindowFunction<User, String, String, TimeWindow>.Context context, Iterable<User> elements, Collector<String> out) throws Exception {
+                long start = context.window().getStart();
+                long end = context.window().getEnd();
+                String windowStart = DateFormatUtils.format(start,"yyyy-MM-dd HH:mm:ss.SSS");
+                String windowEnd = DateFormatUtils.format(end,"yyyy-MM-dd HH:mm:ss.SSS");
+
+                long l = elements.spliterator().estimateSize();
+                System.out.println("存储的数据条数为 " + l);
+                out.collect("key="+s+"的窗口，其开始时间为:【"+windowStart+"，结束时间为:"+windowEnd+"】"+"数据共有： "+l+"条");
+
+
+            }
+        });
+
+        process.print();
+        env.execute();
+
+
+    }
+}
